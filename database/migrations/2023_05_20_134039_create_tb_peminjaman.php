dfc16cab67e4b86f77aa67683cfa7b862efff133
Zidane Dharmawan <mltama1308@gmail.com>
2023-05-20 22:49:00 +0800

    sampe di pengaturan database 👍

---
+++ database/migrations/2023_05_20_134039_create_tb_peminjaman.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_peminjaman', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->char('nama_siswa', 250);
+            $table->string('kelas_siswa', 250);
+            $table->char('nomor_hp', 250);
+            $table->char('judul_buku', 250);
+            $table->date('tanggal_peminjaman');
+            $table->date('tanggal_pengembalian')->nullable();
+            $table->integer('denda')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_peminjaman');
+    }
+};
